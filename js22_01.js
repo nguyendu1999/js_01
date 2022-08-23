de652b4276c98aea5c26d91d2c4fc228c9fb30ee
Nguyen Du <nguyendu07071010@gmail.com>
2022-08-23 14:17:28 +0700

    hii

---
+++ js22_01.js
@@ -0,0 +1,76 @@
+console.log("bai tap 1:");
+console.log("yeu cha me");
+
+console.log("==================");
+
+console.log("bai tap 2:");
+let a=3;
+let b=2;
+let hieu=a/b;
+console.log(hieu);
+
+console.log("==================");
+
+console.log("bài tập 3:");
+let tong=a+b;
+console.log(tong);
+
+console.log("==================");
+
+console.log("bài tập 4:");
+let j=32;
+let nhietdo=(5/9*(j-32));
+console.log(nhietdo);
+
+console.log("==================");
+console.log("bài tập 5:");
+let x=2; y=4;
+let tongbinhphuong=(x**2+y**2);
+console.log(tongbinhphuong);
+
+console.log("==================");
+console.log("bài tập 6:");
+let binhphuong=((x+y)**2);
+console.log(binhphuong);
+
+console.log("==================");
+console.log("bài tập 7:");
+let i=2;let m=4; let n=16;
+let chiabaso=((i**2*m**2)/n);
+console.log(chiabaso);
+
+console.log("==================");
+console.log("bài tập 8:");
+
+let toán=1;Lý=2;Hóa=3;Anh=4;
+let tongdiem=(toán+Lý+Hóa+Anh);
+let diemtrungbinh=(tongdiem/4)
+console.log(diemtrungbinh);
+
+console.log("==================");
+console.log("bài tập 9:");
+
+let f=3;k=0;
+let chiavoikhong=(f/k);
+console.log(chiavoikhong);
+
+console.log("==================");
+console.log("bài tập 10:");
+
+let q=3;w=-1;e=-2
+let nghiem=(-(w)+Math.sqrt((w)**2-4*q*e));
+let bac2=(2*q);
+console.log(nghiem/bac2);
+
+console.log("==================");
+console.log("bài tập 11:");
+
+let t=4;o=2;
+let top=(Math.sqrt(t)+Math.abs(t));
+let buttom=(Math.sqrt(t**o));
+let S=(top/buttom);
+console.log(S);
+
+console.log("==================");
+console.log("bài tập 12:");
+
