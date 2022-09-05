3411b87ccbe6cfc544201e85b7d34f69550fb789
Nguyen Du <nguyendu07071010@gmail.com>
2022-09-05 22:47:05 +0700

    fisth commit

--- bt.js
+++ bt.js
@@ -14,9 +14,105 @@
 // console.log("doan duong la: " , d);
 // console.log("so tien: ",s +"vnd");
 
-function quangduong(xnha,ynha,xkmin,ykmin){
-    let d=Math.sqrt(Math.pow(xkmin-xnha,2)+ Math.pow(ykmin-ynha,2));
-    console.log("doan duong la :", d +"km");
-}
+// function quangduong(xnha,ynha,xkmin,ykmin){
+//     let d=Math.sqrt(Math.pow(xkmin-xnha,2)+ Math.pow(ykmin-ynha,2));
+//     let s=Math.ceil(quangduong)*8000;
+//     console.log("doan duong la :", d +"km");
+//     console.log("Tong tien cua ban la :", s +"Vnd");
+// }
 
-quangduong (4,5,6,7);
+// quangduong (4,5,6,7);
+
+// function tentuoi (){
+//     let d="Nguyen Du";
+//     let e="23";
+//     console.log("toi ten" , d , "toi", e ,"tuoi");
+// }
+
+// tentuoi();
+ 
+// function tinhdungsai(sochan){
+//     if(sochan %2==0){
+//         console.log(sochan,"Day la so chan");
+//     }
+//     else{
+//         console.log(sochan,"day la so le");
+//     }
+// }
+
+// tinhdungsai(4);
+// tinhdungsai(5);
+// tinhdungsai(29934);
+
+// function kiemtranamnhuan(yeah){
+//     if(yeah % 100==0){
+//         if(yeah %400==0){
+//             console.log(yeah , ":" ,"la nam nhaun");
+//         }
+//         else{
+//             console.log(yeah , ":" ,"Khong phai nam nhuan");
+//         }
+//     }
+//     else if(yeah % 4==0){
+//         console.log(yeah ,":" ,"la nam nhuan");
+//     }
+//     else{
+//         console.log(yeah , ":" ,"khong phai nam nhuan");
+//     }
+// }
+
+// kiemtranamnhuan(1999);
+
+// function khiemtranamnhuanhaykhong(yeah){
+//     if(yeah % 100==0){
+//         if (yeah % 400==0){
+//             return true ;
+//         }
+//         else{
+//             return false ;
+//         }
+//     }
+//     else if (yeah % 4==0){
+//         return true ;
+//     }
+//     else{
+//         return flase ;
+//     }
+// }
+
+// let tex=khiemtranamnhuanhaykhong(2010);
+
+// if(tex){
+//     console.log(tex ,": la nam nhuan");
+// }
+// else{
+//     console.log(tex ,": khong phai nam nhuan");
+// }
+
+// console.log("tính diện tích")
+// function dientich(chieudai,chieurong){
+//     let tongdientich=chieudai*chieurong;
+//     return tongdientich;
+// }
+// let dtnhapho=dientich(4,6);
+// let dtmattien=dientich(5,7);
+
+// let tongdt=dtnhapho+dtmattien;
+// console.log("Tổng diện tích là: "+tongdt+"metVuong");
+
+// console.log("kiểu dữ liệu object :thường dùng ở những đối tượng mang nhiều thuộc tính");
+// let student={
+//     name:"Nguyễn Du",
+//     age:23 ,
+//     Sothich: "đàn"
+// };
+ 
+// console.log("tui tên là: " ,student.name+"\ntui năm nay: ",student.age + "\n tui thích chơi:",student.Sothich);
+
+
+let danhsachvieccanlam=[
+    "luyện công","luyện thanh","luyện đàn"
+];
+
+console.log(danhsachvieccanlam[0]+"\n"+danhsachvieccanlam[1]+"\n"+danhsachvieccanlam[2]);
+console.log("số lượng phần tử: " +danhsachvieccanlam.length);
--- js22_01.js
+++ js22_01.js
@@ -1,77 +1,128 @@
+console.log("==================");
+
 console.log("bai tap so1")
 console.log("\"i love you\"");
 
 console.log("==================");
 
 console.log("bai tap 2:");
-let a=3;
-let b=2;
-let hieu=a/b;
-console.log(hieu);
+function bai2() {
+    let a=3;b=2; 
+    console.log("cho nhập vào a=3 và b=2, vạy nếu a chia cho b thì bằng bao nhiêu");
+    let hieu=a/b;
+    console.log("kết quả cuối cùng là: ",hieu);
+}
+bai2();
 
 console.log("==================");
 
 console.log("bài tập 3:");
-let tong=a+b;
-console.log(tong);
+function bai3(){
+    let a=3;b=2; 
+    let tong=a+b;
+    console.log("cho nhập vào a=3 và b=2, vạy nếu a cộng cho b thì bằng bao nhiêu.");
+    console.log("Kết quả cuối cùng là: ",tong);
+}
+bai3();
 
 console.log("==================");
 
 console.log("bài tập 4:");
-let j=32;
-let nhietdo=(5/9*(j-32));
-console.log(nhietdo);
+function bai4(){
+    let j=32;
+    let nhietdo=(5/9*(j-32));
+    console.log("Nhập vào 32 , vạy nếu cho công thức C=5/9(F-30) thì kết quả bằng bao nhiêu.");
+    console.log("Kết quả cuối cùng là: ",nhietdo);
+}
+bai4();
 
 console.log("==================");
+
 console.log("bài tập 5:");
-let x=2; y=4;
-let tongbinhphuong=(x**2+y**2);
-console.log(tongbinhphuong);
+function bai5(){
+    let x=2; y=4;
+    let tongbinhphuong=(x**2+y**2);
+    console.log("cho nhâp vào x=2 và y=4, hãy tính tổng bình phương của 2 số nguyên đó.");
+    console.log("kết quả cuối cùng là: ",tongbinhphuong);
+}
+bai5()
 
 console.log("==================");
+
 console.log("bài tập 6:");
-let binhphuong=((x+y)**2);
-console.log(binhphuong);
+function bai6(){
+    let x=2;y=4;
+    let binhphuong=((x+y)**2);
+    console.log("cho nhâp vào x=2 và y=4, hãy tính bình phương tổng của 2 số nguyên đó.");
+    console.log("Kết quả cuối cùng là:",binhphuong);
+}
+bai6();
 
 console.log("==================");
+
 console.log("bài tập 7:");
-let i=2;let m=4; let n=16;
-let chiabaso=((i**2*m**2)/n);
-console.log(chiabaso);
+function bai7(){
+    let i=2;m=4;n=16;
+    let chiabaso=((i**2*m**2)/n);
+    console.log("nhập vào 3 số nguyên i=2, m=4, n=16. Thực hiện phép chia 3 số, và cho biết kết quả");
+    console.log("Kết quả cuối cùng là: ",chiabaso);
+}
+bai7();
 
 console.log("==================");
+
 console.log("bài tập 8:");
-
-let toán=1;Lý=2;Hóa=3;Anh=4;
-let tongdiem=(toán+Lý+Hóa+Anh);
-let diemtrungbinh=(tongdiem/4)
-console.log(diemtrungbinh);
+function bai8(){
+    let toán=10;Lý=7;Hóa=5;Anh=4;
+    let tongdiem=(toán+Lý+Hóa+Anh);
+    let diemtrungbinh=(tongdiem/4)
+    console.log("nhập vào điểm 4 môn học và tính điểm trung bình của các môn đó.");
+    console.log("điểm trung bình của học viên là: ",diemtrungbinh);
+}
+bai8();
 
 console.log("==================");
+
 console.log("bài tập 9:");
-
-let f=3;k=0;
-let chiavoikhong=(f/k);
-console.log(chiavoikhong);
+function bai9(){
+    let f=3;k=0;
+    let chiavoikhong=(f/k);
+    console.log("Cho phép nhập vào 2 biến dùng để lưu số nguyên: f=3,k=0. Hãy in ra màn hình kết quả phép tính: b/a")
+    console.log(chiavoikhong);
+}
+bai9();
 
 console.log("==================");
+
 console.log("bài tập 10:");
-
-let q=3;w=-1;e=-2
-let nghiem=(-(w)+Math.sqrt((w)**2-4*q*e));
-let bac2=(2*q);
-console.log(nghiem/bac2);
+function bai10(){
+    let q=3;w=-1;e=-2
+    let nghiem=(-(w)+Math.sqrt((w)**2-4*q*e));
+    let bac2=(2*q);
+    console.log("cho nhập vào 3 số nguyên, hãy tính nghiệm phương trình bậc 2.")
+    console.log("Nghiệm bậc 2 của phương trình là: ",nghiem/bac2);
+}
+bai10();
 
 console.log("==================");
+
 console.log("bài tập 11:");
-
-let t=4;o=2;
-let top=(Math.sqrt(t)+Math.abs(t));
-let buttom=(Math.sqrt(t**o));
-let S=(top/buttom);
-console.log(S);
+function bai11(){
+    let t=4;o=2;
+    let top=(Math.sqrt(t)+Math.abs(t));
+    let buttom=(Math.sqrt(t**o));
+    let S=(Math.abs(top)/Math.abs(buttom));
+    console.log("nhập 2 số nguyên t=4 và o=2. Tìm S.")
+    console.log("Kết quả cuối cùng là: ",S);
+}
+bai11();
 
 console.log("==================");
+
 console.log("bài tập 12:");
-let mang=[1,2,3,4,5];
-console.log(mang[4]);
+function bai12(){
+    let mang=[1,2,3,4,5];
+    console.log("Cho nhập vào một mảng. In ra màng hình số 5 của mảng")
+    console.log(mang[4]);
+}
+bai12();
